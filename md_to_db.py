8f0e3384c488c7e6a780e50449f2f73b6566a0fa
mastl <mads.stolberg.larsen@gmail.com>
2016-06-20 02:25:17 +0200

    Notes created with text, title, date and tags.

--- md_to_db.py
+++ md_to_db.py
@@ -24,16 +24,23 @@ identifiers = {
 class Note(object):
 	"""Note object which consists of text."""
 
-	def __init__(self, text, title, date, tags):
-		"""Return a TextSection object whose name is *name*."""
+	def __init__(self, filename):
+		"""Create note from text file."""
+		try:
+			file = open(filename, 'r')
+			text = file.read()
+			file.close()
+		except TypeError:
+			print('File input must be readable.')
+		text_info = extract_info(text)
 		self.text = text
-		self.title = title
-		self.date = date
-		self.tags = tags
+		self.title = text_info['title']
+		self.date = text_info['date']
+		self.tags = text_info['tags']
 
 	def __repr__(self):
 		"""What to print when printing a note."""
-		return "%" % (self.title)
+		return str(self.title)
 
 	def store_to_db(self, db):
 		"""Store object to database."""
@@ -45,6 +52,8 @@ class Note(object):
 
 def first_word(line):
 	"""Isolate first word in line."""
+	if type(line) is not str:
+		print('Input to must be a string.')
 	words = line.split()
 	try:
 		value = words[0]
@@ -55,6 +64,8 @@ def first_word(line):
 
 def extract_info(text):
 	"""Find relevant information in file. I.e. title, date and tags."""
+	if type(text) is not str:
+		print('Input to must be a string.')
 	info = {
 		'title': "",
 		'date': datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S"),
@@ -79,19 +90,10 @@ def extract_info(text):
 	return info  # Return dictionary with title, date and tags!
 
 
-def generate_text(filename):
-	"""Generate text from file."""
-	try:
-		file = open(filename, 'r')
-		text = file.read()
-		file.close()
-	except TypeError:
-		print('File input must be readable.')
-	return text
-
-
 def ignore_identifiers(text_line, possible_identifier):
 	"""Ignore the first word in string if it is an identifier."""
+	if type(text_line) is not str:
+		print('Input to must be a string.')
 	counter = 0
 	for identifier_type in identifiers:
 		counter += 1
@@ -100,12 +102,14 @@ def ignore_identifiers(text_line, possible_identifier):
 			text_line = text_line[start_string:]
 	return text_line
 
+
 # >> Execute <<
+
 filename = 'test_file.md'
-# filename = input('Specify text file: ')
+note = Note(filename)
 
-text = generate_text(filename)
-text_info = extract_info(text)
-
-note = Note(text, text_info)
+print(note.title)
+print(note.date)
+print(note.tags)
 print(note)
+print(note.text)
